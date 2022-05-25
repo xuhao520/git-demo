7e96769f1862515753d1cb9a265dc76378b1cdf8
xuhao520 <2420992758@qq.com>
2022-05-25 13:19:55 +0800

    hot2 commit

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,8 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
+        System.out.println("hello2");
+        System.out.println("hello2");
         System.out.println("hello3");
         System.out.println("hello4");
     }
