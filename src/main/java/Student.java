1d67e63df065e4541b1b566485345880f3dda3f8
xuhao520 <2420992758@qq.com>
2022-05-26 11:02:35 +0800

    2022-5-26

---
+++ src/main/java/Student.java
@@ -0,0 +1,32 @@
+/**
+ * @author xuhao
+ * @date 2022/5/26-10:55
+ */
+public class Student {
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,17 +1,42 @@
+import java.util.Comparator;
+import java.util.TreeSet;
+
 /**
  * @author xuhao
  * @date 2022/5/25-12:09
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("hello1");
-        System.out.println("hello2");
-        System.out.println("hello2");
-        System.out.println("hello2");
-        System.out.println("hello45");
-        System.out.println("hello3");
-        System.out.println("hello4");
-        System.out.println("push");
+//创建集合对象
+        TreeSet<Student> ts = new TreeSet<Student>(new Comparator<Student>
+                () {
+            @Override
+            public int compare(Student s1, Student s2) {
+//this.age - s.age
+//s1,s2
+                int num = s1.getAge() - s2.getAge();
+                int num2 = num == 0 ? s1.getName().compareTo(s2.getName())
+                        : num;
+                return num2;
+            }
+        });
+//创建学生对象
+        Student s1 = new Student("xishi", 29);
+        Student s2 = new Student("wangzhaojun", 28);
+        Student s3 = new Student("diaochan", 30);
+        Student s4 = new Student("yangyuhuan", 33);
+        Student s5 = new Student("linqingxia",33);
+        Student s6 = new Student("linqingxia",33);
+//把学生添加到集合
+        ts.add(s1);
+        ts.add(s2);
+        ts.add(s3);
+        ts.add(s4);
+        ts.add(s5);
+        ts.add(s6);
+//遍历集合
+        for (Student s : ts) {
+            System.out.println(s.getName() + "," + s.getAge());
+        }
     }
 }
